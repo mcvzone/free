7618ad00c43d78535d999da87cbb920a65e4a3bc
mcvzone <mcvzone@naver.com>
2015-11-11 19:30:52 +0900

    add ajax module

---
+++ src/main/java/com/free/module/core/view/AjaxResultView.java
@@ -0,0 +1,33 @@
+package com.free.module.core.view;
+
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+
+import com.free.module.core.config.FreeReservedWordConfig;
+
+import net.sf.json.JSONObject;
+ 
+@Component(value="ajaxResultView")
+public class AjaxResultView extends AbstractView {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AjaxResultView.class);
+	
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		JSONObject jsonResult = (JSONObject)model.get(FreeReservedWordConfig.MISSION_RESULT);
+		
+		PrintWriter out = response.getWriter();
+		out.print(jsonResult);
+	}
+}
