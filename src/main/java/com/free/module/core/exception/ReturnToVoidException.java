d31f3f3ac77320f31b53f9f9de94e6a4121193a4
mcvzone <mcvzone@naver.com>
2015-11-11 19:33:39 +0900

    add module

---
+++ src/main/java/com/free/module/core/exception/ReturnToVoidException.java
@@ -0,0 +1,10 @@
+package com.free.module.core.exception;
+
+public class ReturnToVoidException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public ReturnToVoidException(String sFieldName){
+		super(sFieldName, new Throwable("is null."));
+	}
+}
