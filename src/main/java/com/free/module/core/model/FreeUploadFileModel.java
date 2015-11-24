1548a41d57d1f25b44ff2da3f01c9256046968b7
mcvzone <mcvzone@naver.com>
2015-11-24 17:46:05 +0900

    .

--- src/main/java/com/free/module/core/model/FreeUploadFileModel.java
+++ src/main/java/com/free/module/core/model/FreeUploadFileModel.java
@@ -5,13 +5,12 @@ import org.springframework.web.multipart.MultipartFile;
 public class FreeUploadFileModel {
 
 	private MultipartFile file;
-	// 한번에 다수개의 파일을 받을 때는 List<MultipartFile> files 와 같이 선언하면 된다
-
+	
 	public MultipartFile getFile() {
 		return file;
 	}
 
 	public void setFile(MultipartFile file) {
 		this.file = file;
-	}
+	}	
 }
